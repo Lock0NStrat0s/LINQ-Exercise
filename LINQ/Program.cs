23bcbe9241890bed390a043570ec52e31e0f113c
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-19 09:16:37 -0600

    exercise complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LINQ
+﻿namespace LINQ;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<string> names = new List<string> { "Star Wars: The Force Unleashed", "Final Fantasy", "God of War", "Elden Ring", "Terraria", "Cyberpunk 2077", "Elder Scrolls" };
+
+        // LINQ Method Syntax
+        names = names.OrderBy(x => x.Count()).ToList();
+        foreach (var name in names)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(name);
         }
     }
 }
