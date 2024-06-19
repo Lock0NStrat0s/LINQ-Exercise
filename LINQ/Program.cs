35ed827aa91af081db1d427da54627620b7a85ff
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-19 09:23:07 -0600

    added query syntax for practice

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,7 +6,19 @@ public class Program
     {
         List<string> names = new List<string> { "Star Wars: The Force Unleashed", "Final Fantasy", "God of War", "Elden Ring", "Terraria", "Cyberpunk 2077", "Elder Scrolls" };
 
+        // LINQ Query Syntax
+        Console.WriteLine("Query Syntax:");
+        var nameSpaces = from name in names
+                         orderby name.Count() ascending
+                         select name;
+
+        foreach (var name in nameSpaces)
+        {
+            Console.WriteLine(name);
+        }
+
         // LINQ Method Syntax
+        Console.WriteLine("\nMethod Syntax:");
         names = names.OrderBy(x => x.Count()).ToList();
         foreach (var name in names)
         {
